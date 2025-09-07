490050dedfd9db967c1523c51577ddaaec4b0fd9
Dmitri_Sidorov <kin19861@rambler.ru>
2025-09-07 20:02:41 +0300

    add forms.py/mailing

---
+++ mailing/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import Mailing, Message, Recipient
+
+
+class RecipientForm(forms.ModelForm):
+    class Meta:
+        model = Recipient
+        fields = ["email", "full_name", "comment"]
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ["subject", "body"]
+
+
+class MailingForm(forms.ModelForm):
+    class Meta:
+        model = Mailing
+        fields = ["start_time", "end_time", "message", "recipients"]
